7b6308cbbdf03992e0ab53e15314f5bc1b8da917
Lagats, Aidan <aidan.lagats@csiro.au>
2019-01-21 15:50:56 +1100

    no message

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+  
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 38 },
+        { name: 'Manu', age: 49 },
+      ]
+    });
+  }
+  
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 38 },
+        { name: 'Manu', age: 49 },
+      ]
+    });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi</h1>
+        <button onClick={() => this.switchNameHandler('Mad')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this,'Map')}
+          changed={this.nameChangedHandler}>
+          Hobbies: racing
+        </Person>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}/> 
       </div>
     );
   }
