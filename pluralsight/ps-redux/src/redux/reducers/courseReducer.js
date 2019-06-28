2b4bb511d91e935e3556c380aef3370070987262
Lagats, Aidan (IM&T, North Ryde) <Aidan.Lagats@csiro.au>
2019-06-28 14:33:28 +1000

    no message

--- pluralsight/ps-redux/src/redux/reducers/courseReducer.js
+++ pluralsight/ps-redux/src/redux/reducers/courseReducer.js
@@ -1,8 +1,10 @@
-export default function courseReducer(state, action) {
-    switch (action.type) {
-        case "CREATE_COURSE":
-            return [...state, { ...action.course }];
-        default:
-            return state;
-    }
+import * as types from "../actions/actionTypes";
+
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case types.CREATE_COURSE:
+      return [...state, { ...action.course }];
+    default:
+      return state;
+  }
 }
